7903abcf6daae0c79ed3f8f2c2b8ff45ae72cf34
Jonas Hungershausen <jonas.hungershausen@gmail.com>
2020-01-26 14:52:33 +0100

    test: add homepage tests

---
+++ src/__test__/pages/Homepage.test.tsx
@@ -0,0 +1,33 @@
+import { act, cleanup, fireEvent, render, screen, wait } from '@testing-library/react';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Homepage from '../../pages/Homepage';
+import { MockAuthContextProvider } from '../../__mocks__/authcontext.mock';
+import { api } from '../../util/api';
+import { mockModel } from '../../__mocks__/modelcontext.mock';
+
+afterEach(cleanup);
+
+describe('Homepage', () => {
+  it('renders models if authenticated', async () => {
+    const mockApi = jest.spyOn(api, 'get');
+    const response = new Response(JSON.stringify({ models: [mockModel] }), { status: 200 });
+    mockApi.mockResolvedValue(response);
+
+    await act(async () => {
+      render(
+        <Router>
+          <MockAuthContextProvider isAuthenticated={true}>
+            <Homepage />
+          </MockAuthContextProvider>
+        </Router>
+      );
+      expect(screen.getByText('Loading models...')).toBeDefined();
+    });
+
+    expect(mockApi.mock.calls.length).toBe(1);
+    expect(screen.getByText('model-name')).toBeDefined();
+    expect(screen.getByText('1 Modell')).toBeDefined();
+    mockApi.mockRestore();
+  });
+});
