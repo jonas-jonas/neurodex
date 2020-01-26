56c76d9371fd3d954bd9e10817e0bb9a1a9a06d7
Jonas Hungershausen <jonas.hungershausen@gmail.com>
2020-01-26 13:01:35 +0100

    test: add test for non-ok status code on login

--- src/__test__/components/LoginForm.test.tsx
+++ src/__test__/components/LoginForm.test.tsx
@@ -1,4 +1,4 @@
-import { act, cleanup, fireEvent, render, screen } from '@testing-library/react';
+import { act, cleanup, fireEvent, render, screen, wait } from '@testing-library/react';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import LoginForm from '../../components/LoginForm';
@@ -7,25 +7,22 @@ import { MockAuthContextProvider } from '../../__mocks__/authcontext.mock';
 afterEach(cleanup);
 
 it('Login form sign in trigger authenticate call', async () => {
-  const authenticate = jest.fn(() => {
-    return new Promise<Response>(() => {
-      return {
-        status: 200
-      };
-    });
+  const authenticateMock = jest.fn();
+  authenticateMock.mockResolvedValue({
+    status: 200
   });
   render(
     <Router>
-      <MockAuthContextProvider authenticate={authenticate}>
+      <MockAuthContextProvider authenticate={authenticateMock}>
         <LoginForm />
       </MockAuthContextProvider>
     </Router>
   );
 
-  const userNameField = screen.getByLabelText('Username');
+  const userNameField = screen.getByLabelText('Nutzername');
   expect(userNameField).toBeDefined();
 
-  const passwordField = screen.getByLabelText('Password');
+  const passwordField = screen.getByLabelText('Passwort');
   expect(passwordField).toBeDefined();
 
   await act(async () => {
@@ -40,5 +37,42 @@ it('Login form sign in trigger authenticate call', async () => {
       fireEvent.submit(submitButton.form);
     }
   });
-  expect(authenticate.mock.calls.length).toBe(1);
+  expect(authenticateMock.mock.calls.length).toBe(1);
+});
+
+it('Login form sign in trigger authenticate call', async () => {
+  const authenticateMock = jest.fn();
+  authenticateMock.mockResolvedValue({
+    status: 500,
+    json: () => {
+      return { message: 'Some error message' };
+    }
+  });
+  render(
+    <Router>
+      <MockAuthContextProvider authenticate={authenticateMock}>
+        <LoginForm />
+      </MockAuthContextProvider>
+    </Router>
+  );
+
+  const userNameField = screen.getByLabelText('Nutzername');
+  const passwordField = screen.getByLabelText('Passwort');
+
+  await act(async () => {
+    fireEvent.change(userNameField, { target: { value: 'some-username' } });
+    fireEvent.change(passwordField, { target: { value: 'some-password' } });
+  });
+
+  await act(async () => {
+    // fireEvent.click();
+    const submitButton = (await screen.findByText('Login')) as HTMLButtonElement;
+    if (submitButton.form) {
+      fireEvent.submit(submitButton.form);
+    }
+  });
+  await wait(() => screen.getByText('Some error message'));
+  expect(screen.getByText('Some error message')).toBeDefined();
+
+  expect(authenticateMock.mock.calls.length).toBe(1);
 });
