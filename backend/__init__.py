3deb6a9398710c6cc18c6d88e13c8149346a9137
Jonas Hungershausen <jonas.hungershausen@gmail.com>
2020-03-17 10:50:11 +0100

    Use config file

--- backend/__init__.py
+++ backend/__init__.py
@@ -13,15 +13,10 @@ app = Flask(__name__, static_folder=STATIC_DIR)
 Compress(app)
 bcrypt = Bcrypt(app)
 
-app.config['JWT_SECRET_KEY'] = 'hierkommtnochmalwasgutesreinhaha'
-app.config['JWT_TOKEN_LOCATION'] = ['cookies']
-app.config['JWT_ACCESS_COOKIE_PATH'] = '/api/'
-app.config['JWT_REFRESH_COOKIE_PATH'] = '/api/auth/refresh-token'
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 10
-app.config['JWT_COOKIE_CSRF_PROTECT'] = False
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_pyfile('application.cfg', silent=False)
+profile = os.environ.get('FLASK_ENV')
+if profile is not None:
+    app.config.from_pyfile(f'application-{profile}.cfg', silent=True)
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
